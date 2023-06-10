de1bfab8a96375aef8bef1f6ed9000a4fda130bb
Mohd Aman <saifiamaan445@gmail.com>
2023-06-10 23:20:14 +0530

    async/defer debounce throttle

---
+++ module2/class19/script.js
@@ -0,0 +1,14 @@
+// let body = document.querySelector("body");
+
+// let div = document.createElement("div");
+// div.innerText = "I am a div";
+
+// body.appendChild(div);
+
+let count = 0;
+while(count<100000){
+    // console.log(count++);
+    count++;
+}
+
+console.log("I am script");
