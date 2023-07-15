df10fa598cd38bf2790e1ca9858a64feb4c03507
Mohd Aman <saifiamaan445@gmail.com>
2023-07-15 23:05:20 +0530

    pagination

--- module3/movies-app/src/Components/Banner.jsx
+++ module3/movies-app/src/Components/Banner.jsx
@@ -1,10 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Banner(){
+    let [detail,setDetail] = useState({})
+
+    useEffect(()=>{
+        axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=7a14990db2f038e12eadc4125fe96763')
+        .then(function(res){
+            // console.log(res);
+            
+            console.log(res.data.results[0].poster_path);
+            let data = res.data.results[0]
+            setDetail(data);
+        })
+    },[]);
+
     return(
         <div className="h-[20vh] md:h-[70vh] bg-cover bg-center flex items-end" 
-        style={{backgroundImage:`url(https://assets-in.bmscdn.com/discovery-catalog/events/et00311714-ewdhvajezf-landscape.jpg)`}}>
+        style={{backgroundImage:`url(https://image.tmdb.org/t/p/original/${detail.poster_path})`}}>
             <div className="text-xl text-white bg-gray-900/60 w-full p-4 text-center ">
-                John Wick
+                {detail.title}
             </div>
         </div>
     )
--- module3/movies-app/src/Components/Movies.jsx
+++ module3/movies-app/src/Components/Movies.jsx
@@ -1,18 +1,29 @@
 import MovieCard from "./MovieCard";
 import axios from "axios"
 import {useEffect, useState} from "react"
+import Pagination from "./Pagination";
 
 function Movies(){
     let [movies,setMovies] = useState([]);
-    
+    let [pageNo,setPageNo] = useState(1);
+
+    let handlePrev = ()=>{
+        if(pageNo>1)
+            setPageNo(pageNo-1)
+    }
+
+    let handleNext = ()=>{
+        setPageNo(pageNo+1);
+    }
+
     useEffect(()=>{
-        axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=7a14990db2f038e12eadc4125fe96763`)
+        axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=7a14990db2f038e12eadc4125fe96763&page=${pageNo}`)
         .then(function(res){
         // console.log(res);
         // console.log(res.data.results);
         setMovies(res.data.results);
     })
-    },[])
+    },[pageNo])
 
     
 
@@ -23,10 +34,11 @@ function Movies(){
             </div>
             <div className="flex flex-wrap justify-around gap-8">
                 {movies.map((movieObj)=>{
-                    console.log(movieObj);
+                    // console.log(movieObj);
                     return <MovieCard key={movieObj.id} name={movieObj.title} poster_path={movieObj.poster_path}/>
                 })}
             </div>
+            <Pagination pageNo={pageNo} handleNext={handleNext} handlePrev={handlePrev}/>
         </div>
     )
 }
