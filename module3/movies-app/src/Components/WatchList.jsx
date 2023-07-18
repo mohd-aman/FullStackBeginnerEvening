b9f1608986457988cea922f7ea354a7eaffd8d99
Mohd Aman <saifiamaan445@gmail.com>
2023-07-18 23:16:31 +0530

    updated watchList structure and styling

--- module3/movies-app/src/Components/WatchList.jsx
+++ module3/movies-app/src/Components/WatchList.jsx
@@ -75,15 +75,32 @@ function WatchList(){
         },
       ];
     return(
+        <>
+        <div className="flex justify-center">
+            <div className="flex justify-center items-center w-[15rem] h-[3rem] bg-blue-400 rounded-xl text-white">All Genres</div>
+         </div>
+
+
+         <div className="flex justify-center my-4">
+            <input className="h-[3rem] w-[18rem]
+             border-none outline-none bg-gray-200
+             px-4 text-lg " type="text" placeholder="Search for Movies" />
+        </div>
+
 
         <div className="overflow-hidden rounded-lg shadow-md border border-gray-200 m-8">
             <table className="w-full text-gray-500 text-center ">
                 <thead className="bg-gray-50 text-gray-900 border-b-2">
                     <tr>
                         <th>Name</th>
-                        <th>Ratings</th>
+                        <th className="flex">
+                            <div className="p-2"><i class="fa-solid fa-arrow-up"></i></div>
+                           <div className="p-2">Ratings</div>
+                           <div className="p-2"><i class="fa-solid fa-arrow-down"></i></div> 
+                        </th>
                         <th>Popularity</th>
                         <th>Genre</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody className="text-gray-700">
@@ -96,11 +113,13 @@ function WatchList(){
                         <td>{movieObj.vote_average}</td>
                         <td>{movieObj.popularity}</td>
                         <td>Action</td>
+                        <td className=" text-red-600">Delete</td>
                     </tr>
                 })}
                 </tbody>
             </table>
         </div>
+        </>
     )
 }
 
