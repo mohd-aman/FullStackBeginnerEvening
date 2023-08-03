8fe3832ec1d18758f388ab87c62c88a2e7610583
Mohd Aman <saifiamaan445@gmail.com>
2023-08-03 23:06:20 +0530

    cart component created

--- module3/redux_app/src/Pages/Cart.js
+++ module3/redux_app/src/Pages/Cart.js
@@ -1,7 +1,30 @@
-
+import { useSelector,useDispatch } from "react-redux"
+import { remove } from "../Store/cartSlice";
 
 export default function Cart(){
+    let items = useSelector((state)=>state.cart)
+    let dispatch = useDispatch();
+    console.log(items);
+
+    let handleRemove = (itemId)=>{
+        dispatch(remove(itemId))
+    }
+
     return(
-        <div>I am Cart</div>
+        <div className="cartWrapper">
+            
+            {
+            items.map((item)=>(
+              <div key={item.id} className='cartCard'> 
+                 <img src={item.image}></img>
+                 <h5>{item.title}</h5>
+                 <h5>Price : ${item.price} </h5>
+
+                 <button className='remove-btn' onClick={()=>handleRemove(item.id)}>Remove Item</button>
+              </div>
+            ))
+          }
+           
+        </div>
     )
 }
