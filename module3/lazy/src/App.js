f32d0fb2dea95dd7c75f9ef7ca6b664b90b9f9b3
Mohd Aman <saifiamaan445@gmail.com>
2023-08-10 22:14:12 +0530

    lazy loading

--- module3/lazy/src/App.js
+++ module3/lazy/src/App.js
@@ -1,26 +1,51 @@
 // import {moviesData} from "./data"
-import {useState} from "react"
+import {lazy, Suspense, useState} from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import './App.css';
+import Navbar from "./Components/Navbar";
+// import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+// import Testimonials from "./Pages/Testimonial";
+
+
+// const Home = lazy(()=>import("./Pages/Home"));
+// const Products = lazy(()=>import("./Pages/Products"))
+const About = lazy(()=>import("./Pages/About"))
+const Testimonial = lazy(()=>import("./Pages/Testimonial"))
 
 function App() {
-  let [movie,setMovies] = useState([]);
-  let handleClick = ()=>{
-    import('./data').then((module)=>{
-      console.log(module);
-      setMovies(module.moviesData);
-    }) //dynamic import
+  // let [movie,setMovies] = useState([]);
+  // let handleClick = ()=>{
+  //   import('./data').then((module)=>{
+  //     console.log(module);
+  //     setMovies(module.moviesData);
+  //   }) //dynamic import
     
-  }
+  // }
 
   return (
-    <div>
-      <h1>Movies</h1>
-      <button onClick={handleClick}>Show Movies</button>
-      {movie.map((item)=>{
-        return <div key={item.id}>{item.name}</div>
-      })}
-    </div>
+    // <div>
+    //   <h1>Movies</h1>
+    //   <button onClick={handleClick}>Show Movies</button>
+    //   {movie.map((item)=>{
+    //     return <div key={item.id}>{item.name}</div>
+    //   })}
+    // </div>
+    <Suspense fallback={<h2>...Loading</h2>}>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/products" element={<Products/>}></Route>
+          <Route path="/testimonials" element={<Testimonial/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </Suspense>
+    
+    
   );
 }
 
---
+++ module3/lazy/src/Pages/Testimonial.js
@@ -0,0 +1,7 @@
+
+
+export default function Testimonials(){
+    return(
+        <div>I am Testimonials</div>
+    )
+}
