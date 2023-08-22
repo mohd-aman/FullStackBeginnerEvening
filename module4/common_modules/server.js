8b626b98d3c101109e5588df0de2342e852a5648
Mohd Aman <saifiamaan445@gmail.com>
2023-08-22 09:07:33 +0530

    server created

---
+++ module4/common_modules/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+// console.log(http);
+
+let data = {"name":"Aman"}
+
+const server = http.createServer((req,res)=>{
+    console.log("server has been started");
+    // console.log(req);
+    // console.log(res);
+    // res.end("Hello world")
+    // res.end("<h1>Hello World</h1>");
+    res.setHeader("Content-Type","application/json");
+    res.end(JSON.stringify(data))
+})
+
+server.listen(8080);
