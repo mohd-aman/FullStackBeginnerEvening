9cbf81c9edd199eabfa071adf334f19804a68e82
Mohd Aman <saifiamaan445@gmail.com>
2023-08-24 23:06:44 +0530

    middleware application level route leve and built in

--- module4/class2/server.js
+++ module4/class2/server.js
@@ -3,13 +3,43 @@ const express = require("express");
 // console.log(express);
 const app = express();
 
+//Logger middleware 
+app.use((req,res,next)=>{ // Application Level middleware
+    console.log(req.method,req.ip,req.hostname,new Date());
+    next();
+})
+
+
+//dummy Authentication middleware
+const auth = (req,res,next)=>{
+    console.log(req.query);
+    let password = req.query.password;
+    console.log(password);
+    if(password == '123'){
+        next();
+    }else{
+        res.send("You are not authorized for the requested route")
+    }
+}
+
+//Built in middleware
+app.use(express.static('public'));
+
+// app.use(auth);  // app.use method invokes middlware for all the routes in app.
+
 app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.get("/users/:username",(req,res)=>{
     // res.send('Hello World!')
     // res.send("<h1>Hello world</h1>")
+    console.log(req.params);
     res.send({"type":"GET"})
 })
 
-app.post("/",(req,res)=>{
+//route level middleware works only for particluar routes
+app.post("/",auth ,(req,res)=>{ //Route level middleware
     res.send({"type":"POST"})
 })
 
